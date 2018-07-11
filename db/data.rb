b284f99bf48f3167729ca6cbf262bcb801b271c9
github username <github email address>
2018-07-11 10:52:00 -0400

    get 5 pages of data at once (see db/data.rb)

--- db/data.rb
+++ db/data.rb
@@ -2,25 +2,29 @@ require 'rest-client'
 require 'JSON'
 require 'pry'
 
-ticketmaster_data = RestClient.get('https://app.ticketmaster.com/discovery/v2/events.json?apikey=Jx5LTUJQVff8r3cmh2vUuABLGHjW8Nar')
-ticketmaster_data_page1 = RestClient.get('https://app.ticketmaster.com/discovery/v2/events.json?page=1&size=20&apikey=Jx5LTUJQVff8r3cmh2vUuABLGHjW8Nar')
-ticketmaster_data_page2 = RestClient.get('https://app.ticketmaster.com/discovery/v2/events.json?page=2&size=20&apikey=Jx5LTUJQVff8r3cmh2vUuABLGHjW8Nar')
-ticketmaster_data_page3 = RestClient.get('https://app.ticketmaster.com/discovery/v2/events.json?page=3&size=20&apikey=Jx5LTUJQVff8r3cmh2vUuABLGHjW8Nar')
-ticketmaster_data_page4 = RestClient.get('https://app.ticketmaster.com/discovery/v2/events.json?page=4&size=20&apikey=Jx5LTUJQVff8r3cmh2vUuABLGHjW8Nar')
-ticketmaster_data_page5 = RestClient.get('https://app.ticketmaster.com/discovery/v2/events.json?page=5&size=20&apikey=Jx5LTUJQVff8r3cmh2vUuABLGHjW8Nar')
-ticketmaster_data_page6 = RestClient.get('https://app.ticketmaster.com/discovery/v2/events.json?page=6&size=20&apikey=Jx5LTUJQVff8r3cmh2vUuABLGHjW8Nar')
-ticketmaster_data_page7 = RestClient.get('https://app.ticketmaster.com/discovery/v2/events.json?page=7&size=20&apikey=Jx5LTUJQVff8r3cmh2vUuABLGHjW8Nar')
+def get_data
+  array_of_arrays = []
+  uri = "https://app.ticketmaster.com/discovery/v2/events.json?page=0&size=20&apikey=Jx5LTUJQVff8r3cmh2vUuABLGHjW8Nar"
+  5.times do
+    parsed_data = JSON.parse(RestClient.get(uri))
+    current_page = events_array(parsed_data)
+    array_of_arrays << current_page
+    uri = "https://app.ticketmaster.com" + parsed_data["_links"]["next"]["href"] + "&apikey=Jx5LTUJQVff8r3cmh2vUuABLGHjW8Nar"
+    sleep(1)
+  end
+  array_of_arrays.flatten[0]
+end
 
-parsed_data = JSON.parse(ticketmaster_data_page2)
-
-events_array = parsed_data["_embedded"]["events"].map do |event|
+def events_array(parsed_data)
+  parsed_data["_embedded"]["events"].map do |event|
         {
         name: event["name"],
         location: event["_embedded"]["venues"][0]["postalCode"],
         genre: event["classifications"][0]["genre"]["name"],
-        dateTime: event["dates"]["start"]["dateTime"] 
+        dateTime: event["dates"]["start"]["dateTime"]
                 }
     end
+end
 
     binding.pry
     false
