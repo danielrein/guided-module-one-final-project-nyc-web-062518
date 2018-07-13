5c6897eb758ff6873fb900d1816521752a3dab14
danielrein <34512424+danielrein@users.noreply.github.com>
2018-07-13 14:15:00 -0400

    Update user.rb

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -2,7 +2,9 @@ old_logger = ActiveRecord::Base.logger
 ActiveRecord::Base.logger = nil
 
 class User < ActiveRecord::Base
-    # WTF!!!!!!???????
+    # the line "has_many :programs" is commented out because it didn't function as expected. Cause to be identified..
+    # A "programs" method is defined below as a substitute.
+  
     # has_many :programs
     has_many :events, through: :programs
     has_many :restaurants, through: :programs
